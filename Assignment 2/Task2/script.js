643152b4aa1aff8300c3cfc991a78164fb9d95d1
Maham Khursheed <mahamkhursheed67@gmail.com>
2026-05-17 11:45:34 +0500

    update

--- Assignment 2/Task2/script.js
+++ Assignment 2/Task2/script.js
@@ -3,15 +3,14 @@ for (let i = 2; i <= 50; i += 2) {
     console.log(i);
 }
 console.log("Using while loop:");
-j=2
+let j = 2;
 while(j<=50){
     console.log(j);
     j+=2;
 }
-k=2
+let k=2;
 console.log("Using do-while loop:");
-do{
+do {
     console.log(k);
-}while(k<=50){
-    k+=2;
-}
+    k += 2;
+} while (k <= 50);
